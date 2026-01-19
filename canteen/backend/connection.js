41e6b0f12320169133ff519bb5e412cd01e4aeff
SAYANA-SHABU <144515427+SAYANA-SHABU@users.noreply.github.com>
2026-01-19 23:09:22 +0530

    Update connection.js

--- canteen/backend/connection.js
+++ canteen/backend/connection.js
@@ -1,18 +1,12 @@
-import mongoose from "mongoose";
+ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://canteen608:canteen608@cluster0.emtytzf.mongodb.net/?appName=Cluster0');
-    console.log("MongoDB Connected!");
+    await mongoose.connect('mongodb+srv://canteen608:canteen608@cluster0.emtytzf.mongodb.net/canteen?retryWrites=true&w=majority&appName=Cluster0');
+    console.log("✅ MongoDB Connected!");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
-
-export default connectDB;
-
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://canteen608:canteen608@cluster0.28pmwtc.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0')
-//   .then(() => console.log('Connected!'))
-//   .catch((err)=>console.error("MongoDB connection error:",err))
-  
+export default connectDB; 
